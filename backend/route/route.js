f157f08a92133d8a1bc6c35bd84af3bef2e90cb4
AbdallahYnov <abdallah.merad@ynov.com>
2024-06-06 17:10:53 +0200

    Update route.js

--- backend/route/route.js
+++ backend/route/route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require("../controller/controller.js");
 
-router.get('/Accueil', controllers.getAccueil);
+router.get('/index', controllers.getAccueil);
 
 module.exports = router;
