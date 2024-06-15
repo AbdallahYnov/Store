fb784aa13ef234d36731e97ad7868d43c9e71654
AbdallahYnov <abdallah.merad@ynov.com>
2024-06-15 13:43:03 +0200

    app et path

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const path = require('path'); // Utilisé pour gérer les chemins
 const app = express();
 const port = 4000;
 
 const cors = require('cors');
+const mainRoute = require('./routes/mainRoute'); // Importer votre routeur
 
+// Middleware CORS
 app.use(cors({
     origin: '*'
-})) 
+}));
 
+// Configuration du moteur de vues EJS
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Configuration du répertoire des vues
 
+// Middleware pour servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public'))); // Chemin vers les fichiers statiques
+
+// Middleware pour analyser les données des formulaires
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Pour les données JSON
+
+// Utilisation des routes
+app.use('/', mainRoute); // Routeur pour les routes principales
+
+// Démarrage du serveur
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
 });
