820b6c5672f0e1be36e5a89a9de495735ef3474a
AbdallahYnov <abdallah.merad@ynov.com>
2024-06-15 15:27:26 +0200

    gethome route

--- frontend/routes/mainRoute.js
+++ frontend/routes/mainRoute.js
@@ -4,15 +4,15 @@ const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.getHomePage);
 
-router.get('/products', mainController.getAllProducts);
-router.get('/products/:id', mainController.getProductById);
-router.get('/register', mainController.createUser);
-router.get('/register/done', mainController.registerUser);
-router.get('/login', mainController.getLoginPage);
-router.get('/profile/:id', mainController.getUserById);
-router.get('/orders', mainController.getOrderById);
-router.get('/cart', mainController.getCartPage);
-router.get('/checkout', mainController.getCheckoutPage);
-router.get('/error', mainController.getErrorPage);
+// router.get('/products', mainController.getAllProducts);
+// router.get('/products/:id', mainController.getProductById);
+// router.get('/register', mainController.createUser);
+// router.get('/register/done', mainController.registerUser);
+// router.get('/login', mainController.getLoginPage);
+// router.get('/profile/:id', mainController.getUserById);
+// router.get('/orders', mainController.getOrderById);
+// router.get('/cart', mainController.getCartPage);
+// router.get('/checkout', mainController.getCheckoutPage);
+// router.get('/error', mainController.getErrorPage);
 
 module.exports = router;
